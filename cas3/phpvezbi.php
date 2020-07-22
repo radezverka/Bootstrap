b3863fc0011386b2b54482c100bd49432085572a
radezverka <radekosevski@hotmail.com>
2020-07-22 10:31:05 -0700

    PHP Zadaci

---
+++ cas3/phpvezbi.php
@@ -0,0 +1,21 @@
+<?php
+
+	$ime = "Jon";
+
+	
+
+	$broj = 1;
+	$brojka = 2;
+
+	$zbir = $broj + $brojka;
+
+	
+	$broj1 = 12;
+	$broj2 = 8;
+	$broj3 = 4;
+	$broj4 = 99;
+
+	$vkupen_iznos = ($broj1 * $broj4) / ($broj2 - $broj3);
+
+	echo $vkupen_iznos;
+?>
---
+++ cas3/zadaci.php
@@ -0,0 +1,53 @@
+<?php
+	// Zadaca 1
+	$a = 25;
+	$b = 55;
+
+	$vkupen_iznos = $a / $b;
+
+	//echo $vkupen_iznos;
+
+	// Zadaca 2
+
+	$a = "Hello ";
+	$b = "World";
+
+	//echo $a . $b;
+
+	// Zadaca 3
+
+	$a = "Hello";
+	$b = "World";
+
+	//echo $a . " " . $b;
+
+	// Concat . tocka lepejne
+
+	// Zadaca 4
+
+	//$a = "Hello World From PHP World";
+
+	// Dolzina na string
+	//echo strlen($a);
+
+	// Zadaca 5 
+
+	$a = "Hello World From PHP World";
+
+	//echo strpos(W, $a, 10);
+
+	// Zadaca 6 
+
+	echo "I'm very good at \"programming\", \"Coding\" ";
+
+	echo 'I\'m very good at "programming", "Coding"';
+
+	echo "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).";
+
+	echo 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using \'Content here, content here\', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for \'lorem ipsum\' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
+
+';
+
+
+
+?>
