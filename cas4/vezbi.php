8ec43586c644242e686b7878e20d47ea78274a2f
radezverka <radekosevski@hotmail.com>
2020-07-24 09:19:36 -0700

    Cas 4 php

---
+++ cas4/vezbi.php
@@ -0,0 +1,17 @@
+<?php
+	
+//	$a = "Goce";
+//	$b = "Rade";
+
+//	if ($a == $b) {
+//		echo "Rezultatot e tocen";
+//	}
+//	else {
+//		echo "Rezultatot ne e tocen";
+//	}
+
+	$a = "Rade e super";
+	$b = "Kuvar";
+
+	echo "Denes e ludnica den,no" . " " . $a . " " . $b;
+?>
